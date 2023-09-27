0daf652206c66993fee30313c308d2534ba14a0a
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2023-09-27 17:35:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import os
+import subprocess
 
-system("echo hola")
+# Comando que deseas ejecutar
+comando = "echo hola"
+
+# Ejecutar el comando usando subprocess
+resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+# Imprimir la salida estándar y la salida de error
+print("Salida estándar:")
+print(resultado.stdout)
+
+print("\nSalida de error:")
+print(resultado.stderr)
