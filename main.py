d0ae57bcfed134832e04d40cadb638b6eb62c0bd
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2023-09-27 17:27:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from os import system
+import os
 
-system("python3 -m http.server 8080")
+port = 8080
+while True:
+    # Verificar si el puerto está en uso
+    response = os.system(f"nc -zv 127.0.0.1 {port}")
+
+    if response == 0:
+        # El puerto está en uso, cambia al puerto 8089
+        port = 8089
+    else:
+        # El puerto está disponible, inicia el servidor HTTP
+        os.system(f"python3 -m http.server {port}")
+        break
