5dec868872cc129b29ba2d1c93350f2b577908a0
ivangabriel21 <103368415+ivangabriel21@users.noreply.github.com>
2023-09-27 17:22:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import system
 
 system("curl -O https://raw.githubusercontent.com/ivangabriel21/DependeciaDX/main/gotty")
+system("chmod +x gotty")
 system("./gotty -w --port 8080 /bin/bash")
